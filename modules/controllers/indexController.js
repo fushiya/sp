b5a732008a5e5f9cd7e4c09c861cc6d39b0e5c9a
fushiya <fushiya.mephit@gmail.com>
2019-09-24 21:49:25 +0300

    Complete forum

--- modules/controllers/indexController.js
+++ modules/controllers/indexController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Models = require('../models/all_models');
 
 exports.index = (req, res) => {
-    res.redirect('/home');
+    res.redirect('/forum');
 }
 
 exports.home = (req, res) => {
